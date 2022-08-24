8beb74710085cdf452c7e7ff4a0119d1c6df13b3
95prashar <95prashar@gmail.com>
2022-08-24 18:10:38 +0530

    Small Projects

---
+++ FindingRoot/src/mypackage/AnyRoot.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+import java.util.Scanner;
+
+public class AnyRoot {
+public static void main (String [] args)
+{
+	int base;
+	int power;
+	Scanner obj = new Scanner (System.in);
+	System.out.println("Enter the base value: ");
+	base = obj.nextInt();
+	System.out.println("Enter the power: ");
+	power = obj.nextInt();
+	System.out.println(Math.pow(base, power));
+}
+}
